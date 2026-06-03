2a4863c5a6fc22da8176a2b5a516201512d13ef1
dkile <dkile532@naver.com>
2026-06-03 18:44:04 +0900

    feat: tailwindcss 설정, react-resizable-panel 설치

--- src/ui/app/App.tsx
+++ src/ui/app/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from "@tanstack/react-router";
 
 import { router } from "./router";
 
+import "./app.css";
+
 export default function App() {
   return <RouterProvider router={router} />;
 }
